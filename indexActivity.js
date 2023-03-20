6a59db0ea9941e820c22d173ecd7bbeabd1e0783
JeromeBCS22 <FJA0969@dlsud.edu.ph>
2023-03-20 20:00:28 +0800

    230320

---
+++ indexActivity.js
@@ -0,0 +1,20 @@
+console.log('Hello, Welcome to De La Salle University!\n');
+
+let firstName = 'Jerome';
+let lastName = 'Fernandez';
+let age = 20;
+let hobbies = ['Playing video games', 'Listening to music', 'Watching movies/shows', 'Playing guitar', ''];
+let address = {
+	
+	houseNum: 18,
+	street: 'Fir',
+	subdivision: 'Wood estate Village 2',
+	barangay: 'Molino 3',
+	city: 'Bacoor',
+	province: 'Cavite',
+	zipCode: 4102
+
+}
+
+console.log(firstName, lastName, age, hobbies);
+console.log(address);
